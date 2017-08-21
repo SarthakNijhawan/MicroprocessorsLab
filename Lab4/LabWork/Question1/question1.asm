f72c19f7bc88e8ec72cf0c11e6cbd95ecd7b3ec8
SarthakNijhawan <sarthakn69@gmail.com>
2017-08-22 01:06:57 +0530

    Lab4 completed

--- Lab4/LabWork/Question1/question1.asm
+++ Lab4/LabWork/Question1/question1.asm
@@ -8,42 +8,177 @@ ORG 000H
 ljmp main
 
 ORG 100H
+zeroOut:
+	mov r1,50h ; N
+	mov r0,51h ; Starting Pointer
+	loop_zero:
+		mov @r0,#0
+		inc r0
+		djnz r1, loop_zero
+ret
+
+delay_: 				;This routine takes input from 4FH location
+	using 0
+	push psw
+	mov psw,#08H
+	using 1
+	mov A,4FH 			;This is D
+	mov B,#10 			;Divides D by 2 and multiplies by 20 to convert into seconds
+	mul AB 				;Now A has the total number of times delay must be called
+	mov r0,A 			;Total number of iterations over 50ms stored in A
+	delay1:
+		mov R2,#200
+		back1:
+			mov R1,#0FFH
+			back: djnz R1, back
+		djnz R2, back1
+	djnz r0, delay1
+	pop psw
+ret
+
+write_nibble: 			;Lower nibble in 53h and write location in r0
+		clr c
+		subb A,#0ah
+		jc digit
+			add A,#41h
+			mov @r0,A
+		sjmp comp
+		digit:
+			mov A,53h
+			add A,#30h
+			mov @r0,A
+		comp: ret
+
+bin2ascii:
+	mov r2,50h ; N
+	mov r1,51h ; Read pointer
+	mov r0,52h ; Write pointer	
+	
+	loop:
+		mov A,@r1
+		anl A,#0F0H 		; higher nibble saved , rest all bits to 0
+		swap A
+		mov 53h,A
+		acall write_nibble ; 53 is reserved for its argument parameter
+		inc r0
+		
+		mov A,#0FH
+		anl A,@r1 ; Lower order bits recovered
+		mov 53h,A
+		acall write_nibble
+		inc r0
+		inc r1
+	djnz r2,loop
+ret
+
+;======================================
+;			Main Function
+;======================================
+
 main:
+	mov 50H,#64D
+	mov 51H,#80H
+	acall zeroOut
+	
 	mov r0,#80H
+	mov @r0,#41H
+	inc r0
+	mov @r0,#42H
+	inc r0
+	mov @r0,#50H
+	inc r0
 	mov @r0,#53H
 	inc r0
-	mov @r0,#61H
+	mov @r0,#57H
 	inc r0
-	mov @r0,#72H
+	mov @r0,#3DH
+	
+	mov r0,#90H
+	mov @r0,#52H
 	inc r0
-	mov @r0,#74H
+	mov @r0,#30H
 	inc r0
-	mov @r0,#68H
+	mov @r0,#31H
 	inc r0
-	mov @r0,#61H
+	mov @r0,#32H
 	inc r0
-	mov @r0,#6BH
+	mov @r0,#3DH
+	
+	mov r0,#0A0H
+	mov @r0,#52H
 	inc r0
-	mov @r0,#20H
+	mov @r0,#33H
 	inc r0
-	mov @r0,#4EH
+	mov @r0,#34H
 	inc r0
-	mov @r0,#69H
+	mov @r0,#35H
 	inc r0
-	mov @r0,#6AH
+	mov @r0,#3DH
+	
+	mov r0,#0B0H
+	mov @r0,#52H
 	inc r0
-	mov @r0,#68H
+	mov @r0,#36H
 	inc r0
-	mov @r0,#61H
+	mov @r0,#37H
 	inc r0
-	mov @r0,#77H
+	mov @r0,#53H
 	inc r0
-	mov @r0,#61H
+	mov @r0,#50H
 	inc r0
-	mov @r0,#6EH
-	inc r0
-	mov @r0,#00H
+	mov @r0,#3DH
+
+;======================================
+;	Test Data fed into the registers
+;======================================
+	mov r0,#00
+	mov r1,#10
+	mov r2,#20
+	mov r3,#30
+	mov r4,#40
+	mov r5,#50
+	mov r6,#60
+	mov r7,#70
+	
+;======================================
+;	Copy the stuff to a different array
+;======================================
+	mov 70H,r0
+	mov 71H,r1
+	mov 72H,r2
+	mov 73H,r3
+	mov 74H,r4
+	mov 75H,r5
+	mov 76H,r6
+	mov 77H,r7
+	mov 78H,sp
+	mov 79H,A
+	mov 7AH,B
+	mov 7BH,psw
+	
+	binascii:
+		mov 50H,#3
+		mov 51H,#70H
+		mov 52H,#95H
+		acall bin2ascii
+		
+		mov 50H,#3
+		mov 51H,#73H
+		mov 52H,#0A5H
+		acall bin2ascii
+		
+		mov 50H,#3
+		mov 51H,#76H
+		mov 52H,#0B6H
+		acall bin2ascii
+		
+		mov 50H,#3
+		mov 51H,#79H
+		mov 52H,#86H
+		acall bin2ascii
+	
 	ljmp start
+	
 	
 org 200h
 start:
@@ -51,31 +186,54 @@ start:
 	  mov P1,#00h
 	  ;initial delay for lcd power up
 
-;here1:setb p1.0
       acall delay
-;	  clr p1.0
 	  acall delay
-;	  sjmp here1
-
 
 	  acall lcd_init      ;initialise LCD
 	
 	  acall delay
 	  acall delay
 	  acall delay
-	  mov 	a,#83h		 ;Put cursor on first row,3 column
+	  
+	  ;=====================================
+	  ;				First LCD display
+	  ;=====================================
+	  mov 	a,#80h		 ;Put cursor on first row,0 column
 	  acall lcd_command	 ;send command to LCD
 	  acall delay
-	  mov   dptr,#my_string1   ;Load DPTR with sring1 Addr
-	  acall lcd_sendstring	   ;call text strings sending routine
+	  mov   r0,#80H   		;Load DPTR with sring1 Addr
+	  acall lcd_sendstring_name	   ;call text strings sending routine
 	  acall delay
 
 	  mov a,#0C0h		  ;Put cursor on second row,0 column
 	  acall lcd_command
 	  acall delay
-	  mov   r0,#80H
+	  mov   r0,#90H
 	  acall lcd_sendstring_name
+	  
+	  mov 4FH,#10D
+	  acall delay_
+	  
+	  ;=====================================
+	  ;				Second LCD display
+	  ;=====================================
+	  mov 	a,#80h		 ;Put cursor on first row,0 column
+	  acall lcd_command	 ;send command to LCD
+	  acall delay
+	  mov   r0,#0A0H   		;Load DPTR with sring1 Addr
+	  acall lcd_sendstring_name	   ;call text strings sending routine
+	  acall delay
 
+	  mov   a,#0C0h		  ;Put cursor on second row,0 column
+	  acall lcd_command
+	  acall delay
+	  mov   r0,#0B0H
+	  acall lcd_sendstring_name
+	  
+	  mov 4FH,#10D
+	  acall delay_
+	  
+	  
 here: sjmp here				//stay here 
 
 ;------------------------LCD Initialisation routine----------------------------------------------------
@@ -124,9 +282,9 @@ lcd_init:
          setb  LCD_en         ;Enable H->L
 		 acall delay
          clr   LCD_en
-		 acall delay
-    
+		 acall delay    
          ret  
+		 
 ;-----------------------data sending routine-------------------------------------		     
  lcd_senddata:
          mov   LCD_data,A     ;Move the command to LCD port
@@ -138,17 +296,6 @@ lcd_init:
          acall delay
 		 acall delay
          ret                  ;Return from busy routine
-
-;-----------------------text strings sending routine-------------------------------------
-lcd_sendstring:
-         clr   a                 ;clear Accumulator for any previous data
-         movc  a,@a+dptr         ;load the first character in accumulator
-         jz    exit              ;go to exit if zero
-         acall lcd_senddata      ;send first char
-         inc   dptr              ;increment data pointer
-         sjmp  LCD_sendstring    ;jump back to send the next character
-exit:
-         ret                     ;End of routine
 		 
 ;-----------------------text strings sending routine-------------------------------------
 lcd_sendstring_name:
@@ -156,7 +303,7 @@ lcd_sendstring_name:
          mov   a,@r0         	 ;load the first character in accumulator
          jz    exit2              ;go to exit if zero
          acall lcd_senddata      ;send first char
-         inc   r0              	;increment register
+         inc   r0              ;increment data pointer
          sjmp  LCD_sendstring_name    ;jump back to send the next character
 exit2:
          ret                     ;End of routine
@@ -171,11 +318,5 @@ loop2:	 mov r1,#255
 loop1:	 djnz r1, loop1
 		 djnz r0,loop2
 		 pop psw
-		 ret
-
-;------------- ROM text strings---------------------------------------------------------------
-org 300h
-my_string1:
-         DB   "EE337-Lab2", 00H
+ret
 end
-
--- Lab4/LabWork/Question2/question2.asm
+++ Lab4/LabWork/Question2/question2.asm
@@ -1,84 +1,178 @@
-; This subroutine writes characters on the LCD
+;Display the contents stored at a memloc on the LCD
 LCD_data equ P2    ;LCD Data port
 LCD_rs   equ P0.0  ;LCD Register Select
 LCD_rw   equ P0.1  ;LCD Read/Write
 LCD_en   equ P0.2  ;LCD Enable
 
-ORG 000H
+org 00h
 ljmp main
 
-ORG 100H
+org 100h
+;======================================
+;			Delay Function
+;======================================
+delay_: 				;This routine takes input from 50H location
+	push psw
+	mov psw,#08H
+	using 1
+	mov A,50H 			;This is D
+	mov B,#10 			;Divides D by 2 and multiplies by 20 to convert into seconds
+	mul AB 				;Now A has the total number of times delay must be called
+	mov r0,A 			;Total number of iterations over 50ms stored in A
+	delay1:
+		mov R2,#200
+		back1:
+			mov R1,#0FFH
+			back: djnz R1, back
+		djnz R2, back1
+	djnz r0, delay1
+	pop psw
+ret
+
+;======================================
+;			packNibble Function
+;======================================
+
+readNibble:
+	using 0
+	mov 50H,#10D
+	mov R0,#0
+	loop:
+		mov 4EH,R0
+		mov P1,#0FFH
+		acall delay_
+		anl P1,#0FH
+		mov A,P1
+		mov R0,A
+		swap A
+		orl P1,A
+		acall delay_
+		cjne R0,#0FH,loop
+		
+		mov A,4EH
+		swap A
+		mov P1,A
+		acall delay_
+		mov P1,#00H
+		mov 50H,#2
+		acall delay_
+ret
+
+packNibble:
+	acall readNibble
+	mov A,4EH
+	swap A
+	mov 4FH,A
+	acall readNibble
+	mov A,4EH
+	orl A,4FH
+	mov 4FH,A
+ret
+
+;======================================
+;		bin2ascii subroutine	
+;======================================
+
+write_nibble: 			;Lower nibble in 53h and write location in r0
+		clr c
+		subb A,#0ah
+		jc digit
+			add A,#41h
+			mov @r0,A
+		sjmp comp
+		digit:
+			mov A,53h
+			add A,#30h
+			mov @r0,A
+		comp: ret
+
+bin2ascii:
+	using 1
+		push psw
+		mov psw,#08H
+	mov r2,50h ; N
+	mov r1,51h ; Read pointer
+	mov r0,52h ; Write pointer	
+	
+	binloop:
+		mov A,@r1
+		anl A,#0F0H 		; higher nibble saved , rest all bits to 0
+		swap A
+		mov 53h,A
+		acall write_nibble ; 53 is reserved for its argument parameter
+		inc r0
+		
+		mov A,#0FH
+		anl A,@r1 ; Lower order bits recovered
+		mov 53h,A
+		acall write_nibble
+		inc r0
+		inc r1
+	djnz r2,binloop
+	pop psw
+ret
+
+;===============================================
+;				Main Function
+;===============================================
 main:
-	mov r0,#80H
-	mov @r0,#53H
-	inc r0
-	mov @r0,#61H
-	inc r0
-	mov @r0,#72H
-	inc r0
-	mov @r0,#74H
-	inc r0
-	mov @r0,#68H
-	inc r0
-	mov @r0,#61H
-	inc r0
-	mov @r0,#6BH
-	inc r0
-	mov @r0,#20H
-	inc r0
-	mov @r0,#4EH
-	inc r0
-	mov @r0,#69H
-	inc r0
-	mov @r0,#6AH
-	inc r0
-	mov @r0,#68H
-	inc r0
-	mov @r0,#61H
-	inc r0
-	mov @r0,#77H
-	inc r0
-	mov @r0,#61H
-	inc r0
-	mov @r0,#6EH
-	inc r0
-	mov @r0,#00H
-	ljmp start
+	mov sp,#0C7H
+	mov 12H,#53H
 	
-org 200h
-start:
-      mov P2,#00h
-	  mov P1,#00h
-	  ;initial delay for lcd power up
+	mov P1,#0FH				;First configure switches as input and LED’s as output
+	acall lcd_init			;initialise LCD
+	acall delay
+	acall delay
+	acall delay
 
-;here1:setb p1.0
-      acall delay
-;	  clr p1.0
-	  acall delay
-;	  sjmp here1
-
-
-	  acall lcd_init      ;initialise LCD
+loop_main:
 	
-	  acall delay
-	  acall delay
-	  acall delay
-	  mov 	a,#83h		 ;Put cursor on first row,3 column
-	  acall lcd_command	 ;send command to LCD
-	  acall delay
-	  mov   dptr,#my_string1   ;Load DPTR with sring1 Addr
-	  acall lcd_sendstring	   ;call text strings sending routine
-	  acall delay
+	mov   a,#80h		 	;Put cursor on first row,0 column
+	acall lcd_command	 	;send command to LCD
+	acall delay
+	mov  dptr,#my_string    ;Load DPTR with sring1 Addr
+	acall lcd_sendstring	;call text strings sending routine
+	acall delay
 
-	  mov a,#0C0h		  ;Put cursor on second row,0 column
-	  acall lcd_command
-	  acall delay
-	  mov   r0,#80H
-	  acall lcd_sendstring_name
+	mov 50H,10D				;wait for 5 sec
+	acall delay_
+	
+	acall packNibble		;Read 8 bit value using subroutine packNibbles
+	
+	;;;;;;;;
+	mov 50H,#1				;Parsing the memloc
+	mov 51H,#4FH
+	mov 52H,#60H
+	acall bin2ascii
+	
+	mov 62H,#' '
+	mov r0,4FH
+	mov 4FH,@r0				;Storing the value inside the location stored in 4FH
+	
+	mov 50H,#1				;Parsing the memloc
+	mov 51H,#4FH
+	mov 52H,#63H
+	acall bin2ascii
+	
+	mov 65H,#0H
+	
+	;Display the value of memory address and content in Hex on LCD
+	mov a,#0C5H		  		 ;Put cursor on second row,0 column
+	acall lcd_command
+	acall delay
+	mov   r0,#60H
+	acall lcd_sendstring_name;eg: Display "12 53"  where memory location 12H contains value 53H
+	
+	mov 50H,#10D			 	;wait for 5 sec
+	acall delay_
+	
+	sjmp loop_main				 ;return to loop
+	
+	stop: sjmp stop
 
-here: sjmp here				//stay here 
-
-;------------------------LCD Initialisation routine----------------------------------------------------
+;======================================
+;			LCD subroutines
+;======================================
 lcd_init:
          mov   LCD_data,#38H  ;Function set: 2 Line, 8-bit, 5x7 dots
          clr   LCD_rs         ;Selected command register
@@ -152,7 +246,6 @@ exit:
 		 
 ;-----------------------text strings sending routine-------------------------------------
 lcd_sendstring_name:
-         clr   a                 ;clear Accumulator for any previous data
          mov   a,@r0         	 ;load the first character in accumulator
          jz    exit2              ;go to exit if zero
          acall lcd_senddata      ;send first char
@@ -173,9 +266,7 @@ loop1:	 djnz r1, loop1
 		 pop psw
 		 ret
 
-;------------- ROM text strings---------------------------------------------------------------
 org 300h
-my_string1:
-         DB   "EE337-Lab2", 00H
+	my_string:
+		DB 'Enter memory Location',00H
 end
-
